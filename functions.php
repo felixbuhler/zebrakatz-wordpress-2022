6e028a242475ec10096fb96980652de60a6610d4
felixbuhler <felix.buhler@me.com>
2022-11-08 17:45:17 +0100

    Login Stylesheet

--- functions.php
+++ functions.php
@@ -288,19 +288,3 @@ function my_login_stylesheet() {
     wp_enqueue_script( 'custom-login', get_stylesheet_directory_uri() . '/style-login.js' );
 }
 add_action( 'login_enqueue_scripts', 'my_login_stylesheet' );
-
-function my_login_logo() { ?>
-    <style type="text/css">
-        #login h1 a, .login h1 a {
-            background-image: url();
-		height:65px;
-		width:320px;
-		background-size: 320px 65px;
-		background-repeat: no-repeat;
-        	padding-bottom: 30px;
-        }
-
-        #login h1 a:after, .login h1 a:after {content: "ZEBRA KATZ";}
-    </style>
-<?php }
-add_action( 'login_enqueue_scripts', 'my_login_logo' );
